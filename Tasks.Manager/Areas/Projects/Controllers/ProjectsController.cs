5234222221a47d0461907ca79a3aa34a5d9c0961
BilalSajid-786 <bilalsajid5432@gmail.com>
2025-07-07 22:38:03 +0500

    Feat(Projects): Add the index page for projects to show the list.

---
+++ Tasks.Manager/Areas/Projects/Controllers/ProjectsController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Tasks.Manager.ServiceContracts.ViewModels.Projects;
+
+namespace Tasks.Manager.Areas.Projects.Controllers
+{
+    [Area("Projects")]
+    [Route("[area]/[controller]/[action]")]
+    public class ProjectsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(new List<ProjectViewModel>());
+        }
+    }
+}
